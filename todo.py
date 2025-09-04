be1a99339b0afa44c4bf27e79ceb96a36d0f1b27
OACH <oach.core.sys@outlook.com>
2025-09-04 15:53:18 -0600

    Eliminar importación de colorama en todo.py

--- todo.py
+++ todo.py
@@ -1,5 +1,4 @@
 # 🚀 Importando librerías necesarias
 import json              # 📄 Para leer y escribir archivos JSON
 import argparse          # 💻 Para crear la interfaz CLI
-from colorama import init, Fore, Style  # 🎨 Colores en la terminal
 from pathlib import Path  # 📁 Manejo de rutas de archivos (opcional)
