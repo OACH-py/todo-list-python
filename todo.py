16ae57e876176adeb7717d86ef86fb8576582889
OACH <oach.core.sys@outlook.com>
2025-09-04 16:19:41 -0600

    Eliminar inicialización de colorama en todo.py

--- todo.py
+++ todo.py
@@ -2,8 +2,8 @@
 import json              # 📄 Para leer y escribir archivos JSON
 import argparse          # 💻 Para crear la interfaz CLI
 from pathlib import Path  # 📁 Manejo de rutas de archivos
-from colorama import init, Fore, Style  # 🎨 Para colores en terminal
-init(autoreset=True)
+#from colorama import init, Fore, Style  # 🎨 Para colores en terminal
+#init(autoreset=True)
 
 # 📂 Ruta del archivo JSON que almacena las tareas
 todo = Path("tasks.json")
